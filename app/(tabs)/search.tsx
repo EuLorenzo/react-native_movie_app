458c03113d1671fcec8571b9c226d4489de0a3be
euLorenzo <lorenzo.frinhani@gmail.com>
2025-03-31 10:15:03 -0300

    Appwrite setup done

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -14,6 +14,7 @@ import { fetchMovies } from "@/services/api";
 import { useRouter } from "expo-router";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { updateSearchCount } from "@/services/appwrite";
 
 const search = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -30,6 +31,8 @@ const search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
