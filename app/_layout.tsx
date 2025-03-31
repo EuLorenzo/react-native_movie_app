dcce06d377b191490a460c9f1c76920924abce73
euLorenzo <lorenzo.frinhani@gmail.com>
2025-03-31 12:12:10 -0300

    Trending movies section done on homepage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* Desativa o header que mostra (tabs) */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        {/* Desativa o header que mostra (tabs) */}
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-      {/* Desativa o header que mostra movie/[id] */}
-      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    </Stack>
+        {/* Desativa o header que mostra movie/[id] */}
+        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
